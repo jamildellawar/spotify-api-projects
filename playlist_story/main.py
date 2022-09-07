874c011471a5c91a18b9c1301ae7cd81fc06ea7f
Jamil Dellawar <jamildellawar@dhcp-10-29-52-62.dyn.MIT.EDU>
2022-09-07 16:31:02 -0400

    Create own folder for playlist story and fix bugs

---
+++ playlist_story/main.py
@@ -0,0 +1,50 @@
+import spotipy
+from spotipy.oauth2 import SpotifyAuthBase, SpotifyClientCredentials, SpotifyOAuth
+import os
+# pip install python-dotenv
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SPOTIPY_CLIENT_ID = os.getenv('CLIENT_ID')
+SPOTIPY_CLIENT_SECRET = os.getenv('CLIENT_SECRET')
+# sudo apachectl start
+# if error:
+# sudo apachectl stop
+# or
+# npx kill-port <PORT>
+
+# Just getting all of the scopes so I don't have to worry about switching
+# between it for different projects
+SCOPE_LIST = [
+                'user-modify-playback-state', 
+                'ugc-image-upload', 
+                'user-read-playback-state', 
+                'user-read-private', 
+                'user-follow-modify', 
+                'user-follow-read', 
+                'user-library-modify', 
+                'user-library-read', 
+                'streaming', 
+                'user-read-playback-position', 
+                'playlist-modify-private', 
+                'playlist-read-collaborative', 
+                'app-remote-control', 
+                'user-read-email', 
+                'playlist-read-private', 
+                'user-top-read', 
+                'playlist-modify-public', 
+                'user-read-currently-playing', 
+                'user-read-recently-played'
+                ]
+
+sp = spotipy.Spotify(auth_manager = SpotifyOAuth(scope=SCOPE_LIST, client_id=SPOTIPY_CLIENT_ID, client_secret=SPOTIPY_CLIENT_SECRET))
+
+user_id = sp.current_user()['id']
+
+# Testing (Make sure to be playing something on your Spotify Account)
+# current = sp.current_playback()
+# print("Currently playing " + current['item']['name'] + " from the album " + current['item']['album']['name'] + " by " + current['item']['album']['artists'][0]['name'])
+
+
+
--- playlist_story/playlist_story_create.py
+++ playlist_story/playlist_story_create.py
@@ -1,7 +1,4 @@
-from xml.sax import ContentHandler
 import main
-import helper_functions as hf
-import time
 from spotipy import exceptions
 
 
@@ -18,8 +15,8 @@ def create_playlist(playlistSongs):
     main.sp.playlist_add_items(newPlaylistId, playlistSongs)
 
 def search_for_word(word):
-    counter = 0
     try:
+        counter = 0
         while True:
             searches = main.sp.search(word, type="track", market="US", offset=counter, limit=50)['tracks']['items']
             counter += 50
@@ -28,7 +25,17 @@ def search_for_word(word):
                 if trackName == word:
                     return (track['name'], track['uri'])
     except exceptions.SpotifyException:
-        return None
+        try:
+            counter = 0
+            while True:
+                searches = main.sp.search(f"track:{word}", type="track", market="US", offset=counter, limit=50)['tracks']['items']
+                counter += 50
+                for track in searches:
+                    trackName = track['name'].replace("'","'").replace(".", "").replace(",", "")
+                    if trackName == word:
+                        return (track['name'], track['uri'])
+        except exceptions.SpotifyException:
+            return None
 
 def create_playlist_story():
     phrase = get_phrase()
@@ -36,21 +43,26 @@ def create_playlist_story():
     playlistSongs = []
     previousWords = []
     lostWords = []
+    foundWords = {}
     pointer = 0
     while pointer < len(words):
         if len(previousWords) == 5:
-            pointer -= 3
+            pointer -= 4
             lostWords.append(previousWords[0])
             previousWords = []
             playlistSongs.append(" ")
         previousWords.append(words[pointer])
         phraseToSearch = " ".join(previousWords)
         print(phraseToSearch)
-        searchedWord = search_for_word(phraseToSearch)
+        if phraseToSearch not in foundWords.keys():
+            searchedWord = search_for_word(phraseToSearch)
+        else:
+            searchedWord = foundWords[phraseToSearch]
         if searchedWord:
             playlistSongs.append(searchedWord[1])
             print("found")
             previousWords = []
+            foundWords[searchedWord[0]] = searchedWord
         else:
             print("not found")
         pointer += 1
@@ -58,10 +70,18 @@ def create_playlist_story():
     lostSongCounter = 0
     for song in playlistSongs:
         if song == " ":
-            songURL = input(f"We couldn't find the word \"{lostWords[lostSongCounter]}\". Please enter a song url for this word.")
-            lostSongCounter += 1
-            tempSongURI = main.sp.track(songURL)['uri']
-            playlistSongs.append(tempSongURI)
+            if lostWords[lostSongCounter] not in foundWords.keys():
+                songURL = input(f"We couldn't find the word \"{lostWords[lostSongCounter]}\". Please enter a song url for this word. \n")
+                tempSongURI = main.sp.track(songURL)['uri']
+                tempIndex = playlistSongs.index(song)
+                playlistSongs[tempIndex] = tempSongURI
+                foundWords[lostWords[lostSongCounter]] = tempSongURI
+                lostSongCounter += 1
+            else:
+                tempIndex = playlistSongs.index(song)
+                tempSongURI = foundWords[lostWords[lostSongCounter]]
+                playlistSongs[tempIndex] = tempSongURI
+                lostSongCounter += 1
 
     create_playlist(playlistSongs)
 
